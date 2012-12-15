1e315d7b30fae446671ccce2fc8e3509e730ad87
chap <chapambrose@gmail.com>
2012-12-14 22:17:17 -0600

    add arduino

---
+++ arduino/meatgeek/libraries/HTTPClient/examples/PachubeWiFly/PachubeWiFly.ino
@@ -0,0 +1,101 @@
+/*
+  Pachube sensor client
+ 
+ This sketch connects an analog sensor to Pachube (http://www.pachube.com)
+ using a Wiznet Ethernet shield. You can use the Arduino Ethernet shield, or
+ the Adafruit Ethernet shield, either one will work, as long as it's got
+ a Wiznet Ethernet module on board.
+ 
+ Circuit:
+ * Analog sensor attached to analog in 0
+ * Ethernet shield attached to pins 10, 11, 12, 13
+ 
+ created 15 March 2010
+ updated 4 Sep 2010
+ by Tom Igoe
+ 
+ http://www.tigoe.net/pcomp/code/category/arduinowiring/873
+ This code is in the public domain.
+ 
+ */
+
+#include "WiFly.h"
+#include "Credentials.h"
+
+#include <HTTPClient.h>
+#include <stdio.h>
+#include <Metro.h>
+char buffer[40];
+
+Metro sendingMetro = Metro(60000L);
+
+
+#define SHARE_FEED_URI      "/v2/feeds/10564.csv" 
+
+
+//  The address of the server you want to connect to (pachube.com):
+byte server[] = { 
+  173,203,98,29 }; 
+
+// initialize the library instance:
+EthernetClient client;
+
+long lastConnectionTime = 0;        // last time you connected to the server, in milliseconds
+boolean lastConnected = false;      // state of the connection last time through the main loop
+const int postingInterval = 10000;  //delay between updates to Pachube.com
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Pachube Client");
+  // start the ethernet connection and serial port:
+  WiFly.begin();
+  
+  if (!WiFly.join(ssid, passphrase)) {
+    Serial.println("Association failed.");
+    while (1) {
+      // Hang on failure.
+    }
+  Serial.println("ready");
+}
+
+void loop() {
+  // read the analog sensor:
+  int sensorReading = analogRead(A0);   
+
+
+  // if you're not connected, and ten seconds have passed since
+  // your last connection, then connect again and send data:
+  if(sendingMetro.check() && !client.connected()) {
+    Serial.print("sending ");
+    Serial.println(sensorReading);
+
+    sprintf(buffer, "%d,%d", sensorReading, sensorReading);
+
+    HTTPClient client("api.pachube.com");
+    //client.debug(-1);
+    http_client_parameter pachube_api_header[] = {
+      { 
+        "X-PachubeApiKey","afad32216dd2aa83c768ce51eef041d69a90a6737b2187dada3bb301e4c48841"      }
+      ,{
+        NULL,NULL      }
+    };
+    FILE* result = client.putURI(SHARE_FEED_URI,NULL,buffer, pachube_api_header);
+    int returnCode = client.getLastReturnCode();
+    if (result!=NULL) {
+      client.closeStream(result);
+    } 
+    else {
+      Serial.println("failed to connect");
+    }
+    if (returnCode==200) {
+      Serial.println("data uploaded");
+    } 
+    else {
+      Serial.print("ERROR: Server returned ");
+      Serial.println(returnCode);
+    }
+  }
+  // store the state of the connection for next time through
+  // the loop:
+  lastConnected = client.connected();
+}
---
+++ arduino/meatgeek/meatgeek.ino
@@ -0,0 +1,144 @@
+#include <SPI.h>
+#include <Ethernet.h>
+#include <HTTPClient.h>
+#include <aJSON.h>
+
+// Temperature conversion constants (Steinhart-Hart Equation)
+const float a = 0.000546403;
+const float b = 0.000195178;
+const float c = 0.000000234256;
+
+#define probe 0
+long temps[6] = {0};
+
+//  Ethernet Variables
+byte mac[] = {  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEF };  // MAC address for the ethernet controller.
+byte serverIP[] = { 192,168,1,110 };
+char* serverHost = "meatgeek.192.168.1.110.xip.io";
+#define FEED_URI            "/readings.json"
+EthernetClient client;
+boolean lastConnected = false;
+                                                
+void setup()
+{
+  Serial.begin(9600);
+  analogReference(DEFAULT);
+  
+  Ethernet.begin(mac);
+}
+
+void loop() {
+
+  if(temps[5] == 0)
+  {
+    // add temp to array
+    for (int count=0; count<6; count++)
+    {
+      if (temps[count] == 0)
+      {
+        // store raw resistance
+        temps[count] = analogRead(probe);
+        // Serial.print("resistance=");
+        // Serial.println(temps[count]);
+    
+        // stop looping
+        count = 6;
+      }
+    }
+  }
+  if(temps[5] != 0)
+  {
+    // average and post temp
+    long averageResistance = average(temps);
+    float averageTemp = convertToF(averageResistance);
+    char averageTempString[7];
+    dtostrf(averageTemp, 6, 2, averageTempString);
+    
+//    Serial.print("averageTemp=");
+//    Serial.println(averageTemp);
+    
+    // clear temps array // todo leaky?
+    memset(temps, 0, sizeof(temps));
+    
+    if(!client.connected() ) {
+
+      Serial.print("sending ");
+      HTTPClient client( serverHost, serverIP );
+
+//      couldn't get the json post to work
+//      http_client_parameter meatgeek_APIHeader[] = {
+//        { "X-MeatGeekApiKey", "77"  },
+//        { "Accept", "application/json"},
+//        { "Content-type", "application/json"},
+//        { NULL, NULL }
+//      };
+//      aJsonObject *root,*reading;
+//      root=aJson.createObject();
+//      aJson.addItemToObject(root, "reading", reading = aJson.createObject());
+//      aJson.addNumberToObject(reading,"temperature", averageTemp);  
+//      char *params = aJson.print(root);
+//      FILE* result = client.putURI( FEED_URI, NULL, params, meatgeek_APIHeader );
+
+      http_client_parameter meatgeek_APIHeader[] = {
+        { "X-MeatGeekApiKey", "77"  },
+        { NULL, NULL }
+      };
+      
+      http_client_parameter parameters[] = {
+        { "reading[temperature]", averageTempString},
+        { NULL,NULL }
+      };
+    
+      FILE* result = client.postURI( FEED_URI, parameters, "", meatgeek_APIHeader );
+    
+      int returnCode = client.getLastReturnCode();
+      
+      if (result!=NULL) {
+        client.closeStream(result);  // this is very important -- be sure to close the STREAM
+      } 
+      else {
+        Serial.println("failed to connect");
+      }
+      
+      if (returnCode==200 || returnCode==201) {
+        Serial.println("data uploaded");
+      } 
+      else {
+        Serial.print("ERROR: Server returned ");
+        Serial.println(returnCode);
+      }
+    }
+    
+    
+    // store the state of the connection for next time through
+    // the loop:
+    lastConnected = client.connected();
+  }
+
+  // take temp every 10 seconds
+  delay(1000);
+}
+
+long average(long array[])
+{
+  long sum = 0;
+  int length = sizeof(array);
+  for (int count=0;count<length;count++)
+  {
+    sum += array[count];
+  }
+  
+  return sum / length;
+}
+
+// props to matthew.c.fox@gmail.com
+// http://foxandsqueal.com/?p=45
+float convertToF(long resistance)
+{
+  resistance = resistance*10000/(1024-resistance); //wtf
+  float logcubed=log(resistance);
+  logcubed = logcubed * logcubed * logcubed;
+  float kelvin = 1.0 / (a + b * log(resistance) + c * (logcubed));
+  float temp = (kelvin - 273.15) * 9.0/5.0 + 32.0;
+  return temp;
+}
